6bd1c744d232c9eb8f12041336234ac662d21c1f
CharuTamar <82935831+CharuTamar@users.noreply.github.com>
2025-04-22 20:37:42 +0530

    Update streamlit_app.py
    imported requests library for smoothiefroot api

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,13 @@ if ingredients_list:
     # st.write(my_insert_stmt)
     # st.stop()
 
+   # new section to display smoothiefroot nutrition information
+
+   import requests
+   smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+   st.text(smoothiefroot_response)
+
+
     time_to_insert = st.button('Submit Order')
     
     if time_to_insert:
